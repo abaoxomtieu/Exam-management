46887d4644ae8d769d69495f4291fc3bb43a680d
hotonbao <htbqn2003@gmail.com>
2024-04-13 10:38:15 +0700

    write API for userAnswer detail /:userId/:topicId

--- back-end/controllers/userAnswer.controller.js
+++ back-end/controllers/userAnswer.controller.js
@@ -9,6 +9,25 @@ export const listUserAnswer = async (req, res) => {
     res.status(500).json({ error: err });
   }
 };
+export const listUserAnswerDetail = async (req, res) => {
+  const userId = req.params.userId;
+  const topicId = req.params.topicId;
+  console.log(userId, topicId);
+  try { 
+    const data = await userAnswer.findOne({ userId, topicId });
+
+    if (data) {
+      console.log(data);
+      res.status(200).json(data);
+    } else {
+      res
+        .status(404)
+        .json({ message: "No document found for this user and topic" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
 export const saveUserAnswer = async (req, res) => {
   try {
     console.log("Save User Answer");
--- back-end/routes/userAnswer.route.js
+++ back-end/routes/userAnswer.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { listUserAnswer, saveUserAnswer } from '../controllers/userAnswer.controller.js';
+import { listUserAnswer, listUserAnswerDetail, saveUserAnswer } from '../controllers/userAnswer.controller.js';
 
 const router = express.Router();
 
 router.get('/',listUserAnswer)
+router.get('/:userId/:topicId',listUserAnswerDetail)
 router.post('/',saveUserAnswer)
 
 export default router;
